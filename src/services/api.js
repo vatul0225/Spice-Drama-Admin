e95c7a19d504b0ed82bb39775f81188727d061fe
vatul0225 <atulverma0225@gmail.com>
2026-01-24 07:45:50 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_ADMIN_API}/api`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
